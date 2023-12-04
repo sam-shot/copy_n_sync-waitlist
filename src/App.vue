a8252f9f368a1bc9e7357eafabb6961c75c93f1d
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-12-04 04:54:55 +0100

    fds

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ var isLoading = ref(false);
 var showModal = ref(false);
 
 const joinWaitlist = async () => {
+  if (email.value == "") {
+    showError()
+  }
   isLoading.value = true;
 
   await new Promise(resolve => setTimeout(resolve, 3000));
@@ -13,10 +16,21 @@ const joinWaitlist = async () => {
 
 }
 
+const showError = ()=> {
+
+}
+
+var email = ref('');
+const onEmailChanged = (event) => {
+  email.value = event.target.value;
+  console.log(email.value);
+}
+
 </script>
 
 <template>
   <div class="flex flex-row justify-center text-blue-200 items-center w-screen h-screen">
+    <!-- <div class="p-3 bg-red-600 ">,</div> -->
     <div class="flex flex-col justify-center">
       <img src="./icon.png" alt="" class="mx-auto w-16 mb-10  inline">
       <p class=" text-4xl font-bold text-center mb-12 px-4">Join the waitlist for<br> <span
@@ -29,10 +43,10 @@ const joinWaitlist = async () => {
       </div>
       <div class="w-5/6 relative mb-7 m-auto">
         <div class="absolute h-full flex flex-col justify-center px-3 "><i class="uil uil-envelope "></i></div>
-        <input type="email" autocomplete="email" name="" placeholder="Email address" id=""
+        <input type="email" v-model="email" @input="onEmailChanged" autocomplete="email" name="" placeholder="Email address" id=""
           class="font-medium w-full text-sm bg-neutral-800 pl-9 px-3 py-3 rounded-md bg-transparent border border-neutral-500">
       </div>
-      <div class="w-5/6  m-auto">
+      <div class="w-5/6  m-auto mb-20">
         <button :disabled="isLoading" @click="joinWaitlist()"
           class="hover:transition-all duration-500  transform hover:-translate-y-1  hover:duration-500 py-3 from-green-800 to-cyan-700 text-white font-semibold  w-full rounded-md "
           :class="[!isLoading ? 'bg-gradient-to-r' : '', 'bg-neutral-700 text-neutral-400']"> <span v-if="isLoading"
@@ -51,7 +65,7 @@ const joinWaitlist = async () => {
   <div v-if="showModal"
     class="backdrop-blur-sm flex flex-row justify-center bg-black top-0 bg-opacity-60 absolute text-blue-200 items-center w-screen h-screen">
     <div
-      class="w-1/4 p-7 bg-neutral-900 rounded-2xl from-gray-900 to-neutral-900 flex flex-col items-center  bg-gradient-to-r">
+      class="md:w-1/2 lg:w-2/5 xl:w-2/6 2xl:w-1/4 mx-3 p-7 bg-neutral-900 rounded-2xl from-gray-900 to-neutral-900 flex flex-col items-center  bg-gradient-to-r">
       <div class="flex flex-row justify-end mb-5 w-full">
         <button @click="showModal = false"
           class=" w-9 h-9 bg-gray-800 flex flex-row justify-center items-center rounded-lg "><i
@@ -60,7 +74,7 @@ const joinWaitlist = async () => {
     <img src="./success.png" class="w-32" alt="">
     <p class=" text-2xl font-medium  text-center text-white mb-12 px-4">You have been added to the waitlist!
     </p>
-    <p class=" text-base font-bold  text-center text-neutral-600 mb-12 ">You'll be informed as soon as CopyNSync is
+    <p class=" text-base font-bold  text-center text-neutral-500 mb-12 "><span class="to-white via-cyan-400 from-green-500 bg-gradient-to-r bg-clip-text text-transparent">{{email}}</span> will be informed as soon as CopyNSync is
       ready!
     </p>
   </div>
